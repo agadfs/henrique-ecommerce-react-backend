721a562210244363fba6041cfdcc02a6f041c7e3
agadfs <henriquedefreitasserra@hotmail.com>
2024-01-29 16:34:49 -0300

    v3123

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ import  express  from 'express';
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const cors = require('cors');
 
-app.use(express.json());
-app.use(cors());
+const productsDataPath = './api/user.json'; 
 
-const productsDataPath = './api/user.json';
+app.use('/api/user', (req, res) => {
+    
+    fs.readFile(productsDataPath, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Error reading products data' });
+        } else {
+           
+            const products = JSON.parse(data);
+            res.json(products);
+        }
+    });
+});
 
 
-app.get("/api/user", (req, res) => {
+
+/* app.get("/api/user", (req, res) => {
     
     fs.readFile(productsDataPath, (err, data) => {
         if (err) {
@@ -79,3 +90,4 @@ app.listen(5000, () => {
 });
 
 
+ */
