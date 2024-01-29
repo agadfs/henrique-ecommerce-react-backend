3a5ee8ee68dcc04d0ccfafed5bfe5ce29ea44dc4
agadfs <henriquedefreitasserra@hotmail.com>
2024-01-29 17:42:15 -0300

    vasdas

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const usersFilePath = './api/user.json';
+const usersFilePath = './user.json';
 
 // Route to retrieve user data
 app.get('/api/user', (req, res) => {
