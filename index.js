7c130ac6af2afb2b4c3b237714c836ad9f257701
agadfs <henriquedefreitasserra@hotmail.com>
2024-01-29 16:21:44 -0300

    v322

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const productsDataPath = './api/user';
+const productsDataPath = './api/user.json';
 
 
 app.get("/api/user", (req, res) => {
